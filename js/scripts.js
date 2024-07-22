0f5f563f75462134debe8210b02f2c396784e3d0
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-07-22 18:35:01 +0200

    Esercizio Finito

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,52 @@
-console.log('JS Collegato')
+
+const imgContainer = document.querySelector('.img-container');
+console.log('imgContainer', imgContainer, typeof imgContainer);
+
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+
+];
+
+for (let i = 0; i < images.length; i++) {
+
+    if (i == 0){
+        imgContainer.innerHTML += `<img src="${images[i]}" class="active">`; 
+    }
+    else{
+        imgContainer.innerHTML += `<img src="${images[i]}">`;
+    }
+}  
+
+let imgActive = 1;
+
+const nextButton = document.getElementById('next-button');
+nextButton.addEventListener('click', function() {
+
+    if (imgActive < images.length){
+        document.querySelector('.img-container > img:nth-child(' + imgActive + ')').classList.remove('active');
+
+        imgActive++;
+
+        document.querySelector('.img-container > img:nth-child(' + imgActive + ')').classList.add('active');
+    }
+    
+});
+
+const prevButton = document.getElementById('pre-button');
+prevButton.addEventListener('click', function() {
+
+    if (imgActive > 1){
+
+        document.querySelector('.img-container > img:nth-child(' + imgActive + ')').classList.remove('active');
+
+        imgActive--;
+
+        document.querySelector('.img-container > img:nth-child(' + imgActive + ')').classList.add('active');
+    }
+});
+
+
